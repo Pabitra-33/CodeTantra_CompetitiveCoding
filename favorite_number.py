cca53ed8e10d4ed6d26d604e0be02cb80055d520
Pabitra33 <kumarpabitra345@gmail.com>
2023-11-07 00:55:49 +0530

    2nd commit

--- favorite_number.py
+++ favorite_number.py
@@ -1,1 +1,18 @@
-Mr. Tracy's favorite number
+def is_Tracys_favorite_number(num):
+    def get_next_number(n):
+        total = 0
+        while n > 0:
+            total = total + (n%10) **2
+            num = num//10
+        return total
+
+    while n! = 1 and n not in(89,145,42,20,4,16,37,58):
+        num = get_next_number(num)
+
+    return num == 1
+
+number = int(input())
+if is_Tracys_favorite_number(number):
+    print("Yes")
+else:
+    print("No")
