9f1285ac59769355985efd8ab64b5223b434001d
Pabitra33 <kumarpabitra345@gmail.com>
2023-11-07 19:45:14 +0530

    3rd commit

--- Climb_the_stair.cpp
+++ Climb_the_stair.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int fibo(int n)
+{
+    if(n <= 1){
+        return n;
+    }
+    return fibo(n-1) + fibo(n-2);
+}
+
+int countWays(int steps){
+    return fibo(steps+1);
+}
+
+int main(){
+    int steps;
+    cin >> steps;
+    cout<<countWays(steps);
+    return 0;
+}
