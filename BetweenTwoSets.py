c7d36714e013b9d5fd2002c31df4dc37349d2058
Pabitra33 <kumarpabitra345@gmail.com>
2023-11-09 00:01:58 +0530

    1st commit

--- BetweenTwoSets.py
+++ BetweenTwoSets.py
@@ -1,1 +1,9 @@
-Algorithm problem
+input();
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+result = 0
+for i in range(1, 101):
+    if all(i%x == 0 for x in a) and all(x%i == 0 for x in b):
+        result += 1
+print(result)
